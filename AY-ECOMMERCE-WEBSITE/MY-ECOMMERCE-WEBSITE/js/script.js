918e5e45bfe5144729f25f4cc481db21ff8cc7a8
ayesha-yt <168291881+ayesha-yt@users.noreply.github.com>
2025-04-15 01:38:35 +0500

    Ecommerce-Website
    Developer's Hub Corporation ecommerce website task

---
+++ AY-ECOMMERCE-WEBSITE/MY-ECOMMERCE-WEBSITE/js/script.js
@@ -0,0 +1,24 @@
+/////// Enable tooltip of Bootstrap5
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+/////// Prevent closing from click inside dropdown
+document.querySelectorAll('.dropdown-menu').forEach(function(element) {
+    element.addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
+});
+// end querySelectorAll
+
+
+// prevent empty links with # from scrolling top
+var links = document.links;
+var linksCount = links.length;
+for (var i = 0; i < linksCount; i++) {
+    var link = links[i].attributes.href.value; // prodam garage
+    if (link == "" || link == "#") {
+        links[i].setAttribute("onclick", "return false;");
+    }
+}
